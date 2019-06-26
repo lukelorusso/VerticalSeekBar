08b76372422b358a8c02eb6f665e3f0f5d8e49f4
Luca Lorusso <llorusso@mystudiofactory.com>
2019-06-26 14:24:46 +0200

    Now you can set attributes programmatically

--- verticalseekbar-example/src/main/kotlin/com/lukelorusso/verticalseekbarsample/MainActivity.kt
+++ verticalseekbar-example/src/main/kotlin/com/lukelorusso/verticalseekbarsample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lukelorusso.verticalseekbarsample
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,10 @@ class MainActivity : AppCompatActivity() {
             mainProgressValue.text = progressValue.toString()
         }
         mainProgressValue.text = mainVerticalSeekBar.progress.toString()
+        Handler().postDelayed({
+            mainVerticalSeekBar.clickToSetProgress = true
+            Toast.makeText(this, "Now you can click on drawable to set progress!", Toast.LENGTH_LONG).show()
+        }, 5000)
     }
 
 }
--- verticalseekbar/src/main/kotlin/com/lukelorusso/verticalseekbar/VerticalSeekBar.kt
+++ verticalseekbar/src/main/kotlin/com/lukelorusso/verticalseekbar/VerticalSeekBar.kt
@@ -30,23 +30,86 @@ open class VerticalSeekBar constructor(context: Context, attrs: AttributeSet) : 
 
     private var onProgressChangeListener: ((Int) -> Unit)? = null
 
-    private var clickToSetProgress = true
-    private var drawableCornerRadius: Int = 0
-    private var drawableBackgroundDrawable: Drawable? = null
-    private var drawableProgressDrawable: Drawable? = null
-    private var drawableProgressStartColor: Int = Color.parseColor(DEFAULT_DRAWABLE_PROGRESS_START)
-    private var drawableProgressEndColor: Int = Color.parseColor(DEFAULT_DRAWABLE_PROGRESS_END)
-    private var drawableWidth: Int? = null
-    private var minLayoutWidth: Int = 0
-    private var minLayoutHeight: Int = 0
-    private var maxPlaceholderDrawable: Drawable? = null
-    private var minPlaceholderDrawable: Drawable? = null
-    private var showThumb = true
-    private var thumbContainerColor: Int = Color.WHITE
-    private var thumbContainerCornerRadius: Int = 0
-    private var thumbPlaceholderDrawable: Drawable? = null
-    private var useThumbToSetProgress = true
-    private var yDelta: Int = 0
+    var clickToSetProgress = true
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableCornerRadius: Int = 0
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableBackgroundDrawable: Drawable? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableProgressDrawable: Drawable? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableProgressStartColor: Int = Color.parseColor(DEFAULT_DRAWABLE_PROGRESS_START)
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableProgressEndColor: Int = Color.parseColor(DEFAULT_DRAWABLE_PROGRESS_END)
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var drawableWidth: Int? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var minLayoutWidth: Int = 0
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var minLayoutHeight: Int = 0
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var maxPlaceholderDrawable: Drawable? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var minPlaceholderDrawable: Drawable? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var showThumb = true
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var thumbContainerColor: Int = Color.WHITE
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var thumbContainerCornerRadius: Int = 0
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var thumbPlaceholderDrawable: Drawable? = null
+        set(value) {
+            field = value
+            applyAttributes()
+        }
+    var useThumbToSetProgress = true
+        set(value) {
+            field = value
+            applyAttributes()
+        }
     var progress: Int = 50
         set(value) {
             if (field != value) {
@@ -55,6 +118,8 @@ open class VerticalSeekBar constructor(context: Context, attrs: AttributeSet) : 
             field = value
             updateViews(value)
         }
+    private var yDelta: Int = 0
+    private var initEnded = false // if true allows the view to be updated after setting an attribute programmatically
 
     init {
         init(context, attrs)
@@ -130,106 +195,8 @@ open class VerticalSeekBar constructor(context: Context, attrs: AttributeSet) : 
             attributes.recycle()
         }
 
-        // Customizing drawableCardView
-        drawableCardView.layoutParams.width = drawableWidth ?: 0
-
-        // Customizing drawableBackground
-        drawableBackground.background =
-            drawableBackgroundDrawable ?: ColorDrawable(Integer.decode(DEFAULT_DRAWABLE_BACKGROUND))
-
-        // Generating drawableProgress gradient
-        if (drawableProgressDrawable == null) drawableProgressDrawable = GradientDrawable(
-            GradientDrawable.Orientation.TOP_BOTTOM,
-            intArrayOf(drawableProgressStartColor, drawableProgressEndColor)
-        ).apply { cornerRadius = 0f }
-        drawableProgress.background = drawableProgressDrawable
-
-        // Applying card corner radius
-        drawableCardView.radius = drawableCornerRadius.toFloat()
-        thumbCardView.radius = thumbContainerCornerRadius.toFloat()
-
-        // Applying custom placeholders
-        maxPlaceholder.setImageDrawable(maxPlaceholderDrawable) // can also be null
-        minPlaceholder.setImageDrawable(minPlaceholderDrawable) // can also be null
-
-        // Let's shape the thumb
-        if (showThumb) {
-            thumbPlaceholderDrawable?.also { thumbPlaceholder.setImageDrawable(it) } // CANNOT be null
-            thumbCardView.visibility = View.VISIBLE
-            val states = arrayOf(
-                intArrayOf(android.R.attr.state_enabled), // enabled
-                intArrayOf(-android.R.attr.state_enabled), // disabled
-                intArrayOf(-android.R.attr.state_checked), // unchecked
-                intArrayOf(android.R.attr.state_pressed)  // pressed
-            )
-            val colors = arrayOf(
-                thumbContainerColor,
-                thumbContainerColor,
-                thumbContainerColor,
-                thumbContainerColor
-            ).toIntArray()
-            thumbCardView.backgroundTintList = ColorStateList(states, colors)
-            thumbCardView.measure(0, 0)
-            thumb.layoutParams.apply {
-                val increase = (thumbCardView.elevation + context.dpToPixel(1F)).roundToInt()
-                width = thumbCardView.measuredWidth + increase
-                height = thumbCardView.measuredHeight + increase
-                (thumbCardView.layoutParams as LayoutParams).topMargin = increase / 2
-            }
-        }
-        else thumbCardView.visibility = View.GONE
-
-        // Adding some margin to drawableCardView, maxPlaceholder and minPlaceholder
-        maxPlaceholder.measure(0, 0)
-        minPlaceholder.measure(0, 0)
-        (drawableCardView.layoutParams as LayoutParams).apply {
-            val thumbCardViewHalfHeight = if (showThumb) thumbCardView.measuredHeight / 2 else 0
-            val maxPlaceholderHalfHeight = maxPlaceholder.measuredHeight / 2
-            val minPlaceholderHalfHeight = minPlaceholder.measuredHeight / 2
-            topMargin = max(thumbCardViewHalfHeight, maxPlaceholderHalfHeight)
-            bottomMargin = max(thumbCardViewHalfHeight, minPlaceholderHalfHeight)
-            (maxPlaceholder.layoutParams as LayoutParams).topMargin = topMargin - maxPlaceholderHalfHeight
-            (maxPlaceholder.layoutParams as LayoutParams).bottomMargin = topMargin - maxPlaceholderHalfHeight
-            (minPlaceholder.layoutParams as LayoutParams).bottomMargin = bottomMargin - minPlaceholderHalfHeight
-            (minPlaceholder.layoutParams as LayoutParams).topMargin = bottomMargin - minPlaceholderHalfHeight
-
-        }
-
-        // Here's where the magic happens
-        if (showThumb && useThumbToSetProgress) thumb.setOnTouchListener { thumb, event ->
-            val rawY = event.rawY.roundToInt()
-            when (event.action and MotionEvent.ACTION_MASK) {
-                MotionEvent.ACTION_DOWN -> { // here we calculate the displacement (yDelta)
-                    yDelta = rawY - (thumb.layoutParams as LayoutParams).topMargin
-                }
-                MotionEvent.ACTION_MOVE -> { // here we update progress
-                    val positionY = rawY - yDelta
-                    val fillHeight = height - thumb.height
-                    when {
-                        positionY in 1 until fillHeight -> progress = 100 - (positionY * 100 / fillHeight)
-                        positionY <= 0 -> progress = 100
-                        positionY >= fillHeight -> progress = 0
-                    }
-                }
-            }
-            true
-        } else thumb.setOnTouchListener(null)
-
-        // here we intercept the click on the drawable
-        if (clickToSetProgress) drawableCardView.setOnTouchListener { drawable, event ->
-            val positionY = event.y.roundToInt()
-            when (event.action and MotionEvent.ACTION_MASK) {
-                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
-                    val fillHeight = drawable.measuredHeight
-                    when {
-                        positionY in 1 until fillHeight -> progress = 100 - (positionY * 100 / fillHeight)
-                        positionY <= 0 -> progress = 100
-                        positionY >= fillHeight -> progress = 0
-                    }
-                }
-            }
-            true
-        } else drawableCardView.setOnTouchListener(null)
+        initEnded = true
+        applyAttributes()
     }
 
     fun setOnProgressChangeListener(listener: ((Int) -> Unit)?) {
@@ -243,6 +210,114 @@ open class VerticalSeekBar constructor(context: Context, attrs: AttributeSet) : 
     protected fun Context.pixelToDp(px: Float): Float =
         px / (resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
     //endregion
+
+    private fun applyAttributes() {
+        if (initEnded) {
+            initEnded = false // will be released at the end
+
+            // Customizing drawableCardView
+            drawableCardView.layoutParams.width = drawableWidth ?: 0
+
+            // Customizing drawableBackground
+            drawableBackground.background =
+                drawableBackgroundDrawable ?: ColorDrawable(Integer.decode(DEFAULT_DRAWABLE_BACKGROUND))
+
+            // Generating drawableProgress gradient
+            if (drawableProgressDrawable == null) drawableProgressDrawable = GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                intArrayOf(drawableProgressStartColor, drawableProgressEndColor)
+            ).apply { cornerRadius = 0f }
+            drawableProgress.background = drawableProgressDrawable
+
+            // Applying card corner radius
+            drawableCardView.radius = drawableCornerRadius.toFloat()
+            thumbCardView.radius = thumbContainerCornerRadius.toFloat()
+
+            // Applying custom placeholders
+            maxPlaceholder.setImageDrawable(maxPlaceholderDrawable) // can also be null
+            minPlaceholder.setImageDrawable(minPlaceholderDrawable) // can also be null
+
+            // Let's shape the thumb
+            if (showThumb) {
+                thumbPlaceholderDrawable?.also { thumbPlaceholder.setImageDrawable(it) } // CANNOT be null
+                thumbCardView.visibility = View.VISIBLE
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_enabled), // enabled
+                    intArrayOf(-android.R.attr.state_enabled), // disabled
+                    intArrayOf(-android.R.attr.state_checked), // unchecked
+                    intArrayOf(android.R.attr.state_pressed)  // pressed
+                )
+                val colors = arrayOf(
+                    thumbContainerColor,
+                    thumbContainerColor,
+                    thumbContainerColor,
+                    thumbContainerColor
+                ).toIntArray()
+                thumbCardView.backgroundTintList = ColorStateList(states, colors)
+                thumbCardView.measure(0, 0)
+                thumb.layoutParams.apply {
+                    val increase = (thumbCardView.elevation + context.dpToPixel(1F)).roundToInt()
+                    width = thumbCardView.measuredWidth + increase
+                    height = thumbCardView.measuredHeight + increase
+                    (thumbCardView.layoutParams as LayoutParams).topMargin = increase / 2
+                }
+            } else thumbCardView.visibility = View.GONE
+
+            // Adding some margin to drawableCardView, maxPlaceholder and minPlaceholder
+            maxPlaceholder.measure(0, 0)
+            minPlaceholder.measure(0, 0)
+            (drawableCardView.layoutParams as LayoutParams).apply {
+                val thumbCardViewHalfHeight = if (showThumb) thumbCardView.measuredHeight / 2 else 0
+                val maxPlaceholderHalfHeight = maxPlaceholder.measuredHeight / 2
+                val minPlaceholderHalfHeight = minPlaceholder.measuredHeight / 2
+                topMargin = max(thumbCardViewHalfHeight, maxPlaceholderHalfHeight)
+                bottomMargin = max(thumbCardViewHalfHeight, minPlaceholderHalfHeight)
+                (maxPlaceholder.layoutParams as LayoutParams).topMargin = topMargin - maxPlaceholderHalfHeight
+                (maxPlaceholder.layoutParams as LayoutParams).bottomMargin = topMargin - maxPlaceholderHalfHeight
+                (minPlaceholder.layoutParams as LayoutParams).bottomMargin = bottomMargin - minPlaceholderHalfHeight
+                (minPlaceholder.layoutParams as LayoutParams).topMargin = bottomMargin - minPlaceholderHalfHeight
+
+            }
+
+            // Here's where the magic happens
+            if (showThumb && useThumbToSetProgress) thumb.setOnTouchListener { thumb, event ->
+                val rawY = event.rawY.roundToInt()
+                when (event.action and MotionEvent.ACTION_MASK) {
+                    MotionEvent.ACTION_DOWN -> { // here we calculate the displacement (yDelta)
+                        yDelta = rawY - (thumb.layoutParams as LayoutParams).topMargin
+                    }
+                    MotionEvent.ACTION_MOVE -> { // here we update progress
+                        val positionY = rawY - yDelta
+                        val fillHeight = height - thumb.height
+                        when {
+                            positionY in 1 until fillHeight -> progress = 100 - (positionY * 100 / fillHeight)
+                            positionY <= 0 -> progress = 100
+                            positionY >= fillHeight -> progress = 0
+                        }
+                    }
+                }
+                true
+            } else thumb.setOnTouchListener(null)
+
+            // here we intercept the click on the drawable
+            if (clickToSetProgress) drawableCardView.setOnTouchListener { drawable, event ->
+                val positionY = event.y.roundToInt()
+                when (event.action and MotionEvent.ACTION_MASK) {
+                    MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                        val fillHeight = drawable.measuredHeight
+                        when {
+                            positionY in 1 until fillHeight -> progress = 100 - (positionY * 100 / fillHeight)
+                            positionY <= 0 -> progress = 100
+                            positionY >= fillHeight -> progress = 0
+                        }
+                    }
+                }
+                true
+            } else drawableCardView.setOnTouchListener(null)
+
+            initEnded = true
+        }
+    }
 
     /**
      * Inside here the views are repositioned based on the new value
